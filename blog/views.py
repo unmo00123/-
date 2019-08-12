8ea3ff221911bf37f019c748bc149d15e0ffb0f3
AsukaruShouhei <asukaru2014@icloud.com>
2019-08-13 02:40:34 +0900

    ORM

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,17 @@ def PostForm(POST):
 
 
 def post_new(request):
+    # 以下のソースの意味は、データがPOSTデータで飛んできたときは、saveメソッドでデータを登録する処理です。
+    # そして、データを登録しようとするときにだけ、posts = Post.objects.filter(published_date__lte=
+    # timezone.now()).order_by('published_date')が稼働します。当然単純にページを表示させるときには
+    # このif文の中は通りません。そのため、データが表示されませんでした。
+    # 現在全体的な仕様としてはaccountsの中のviews.py(indexViewメソッド)を通しているように見受けられました。
+    # 細かい設定（仕様）などが現状どうなっているのかわからなかったので、accounts.viewsでblog.modelsをimport
+    # することができませんでした。
+    # 要は、viewsでORマッパーを使用したDB処理を行い、その結果をフロントに返すことで対応できます。
+    # そのため、今回のシステム仕様でいえばaccounts.viewsでblog.modelsをインポートしてきてORマッパーを記述し、
+    # 取得データをフロントに返すことができれば一応目的は果たせると思います。が、出来ればディレクトリ構造は統一性、
+    # 一貫性などをもたせることをおススメします。
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
